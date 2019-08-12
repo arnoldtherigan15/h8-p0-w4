775ada0902c704b60ef0ebf67654abe377b84b84
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-12 18:12:28 +0700

    fixed excercise 7

--- excercise-7.js
+++ excercise-7.js
@@ -1,5 +1,22 @@
 function urutkanAbjad(str) {
-    // you can only write your code here!
+    var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    var strArray = str.split('');
+    var resultNumber = [];
+    var resultString = [];
+    for (var i = 0; i < alphabet.length; i++) {
+        for (var j = 0; j < strArray.length; j++) {
+            if (strArray[j] === alphabet[i]) {
+                resultNumber.push(i);
+            }
+        }
+    }
+    resultNumber.sort(function(a,b){
+        a < b;
+    });
+    for (var k = 0; k < resultNumber.length; k++) {
+        resultString += alphabet[resultNumber[k]];
+    }
+    return resultString;
 }
   
 // TEST CASES
