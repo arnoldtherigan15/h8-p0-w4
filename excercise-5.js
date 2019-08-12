2deeeb67f3800a48fc495c73ec377c82bd7ca017
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-12 16:09:02 +0700

    fixed excercise 5

--- excercise-5.js
+++ excercise-5.js
@@ -1,10 +1,24 @@
 function ubahHuruf(kata) {
-    // you can only write your code here!
-  }
+    var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    var kataBaru = [];
+    var arrKata = [];
+    arrKata = kata.split('');
+    for (var i = 0; i < alphabet.length; i++) {
+        for (var j = 0; j < arrKata.length; j++) {
+            if (alphabet[i] === arrKata[j]){
+                arrKata.splice(j,1,i+1);
+            }
+        }
+    }
+    for (var k = 0; k < arrKata.length; k++) {
+        kataBaru += alphabet[arrKata[k]];
+    }
+    return kataBaru;
+}
   
-  // TEST CASES
-  console.log(ubahHuruf('wow')); // xpx
-  console.log(ubahHuruf('developer')); // efwfmpqfs
-  console.log(ubahHuruf('javascript')); // kbwbtdsjqu
-  console.log(ubahHuruf('keren')); // lfsfo
-  console.log(ubahHuruf('semangat')); // tfnbohbu
+// TEST CASES
+console.log(ubahHuruf('wow')); // xpx
+console.log(ubahHuruf('developer')); // efwfmpqfs
+console.log(ubahHuruf('javascript')); // kbwbtdsjqu
+console.log(ubahHuruf('keren')); // lfsfo
+console.log(ubahHuruf('semangat')); // tfnbohbu
