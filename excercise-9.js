f1594c36a5902a974b617c2489acb68d4aab6dd7
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-12 19:41:08 +0700

    fixed ex 9

--- excercise-9.js
+++ excercise-9.js
@@ -1,6 +1,26 @@
-function checkAB(num) {
-    // you can only write your code here!
-}
+function checkAB(string) {
+    var indeksA = [];
+    var indeksB = [];
+    var selisih = [];
+    for (var i = 0; i < string.length; i++) {
+        if (string[i] === 'a') {
+            indeksA.push(i);
+        } else if (string[i] === 'b') {
+            indeksB.push(i);
+        }
+    }
+    for (var j = 0; j < indeksA.length; j++) {
+        for (var k = 0; k < indeksB.length; k++) {
+            selisih.push(indeksA[j] - indeksB[k]);
+        }
+    }
+    for (var l = 0; l < selisih.length; l++) {
+        if (selisih[l] === 4 || selisih[l] === -4) {
+            return true;
+        }
+    }
+    return false;
+}  
   
 // TEST CASES
 console.log(checkAB('lane borrowed')); // true
