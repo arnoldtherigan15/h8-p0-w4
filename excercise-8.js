144c1307a9ce7e8525b149aba2795cfbf47fc154
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-12 18:50:58 +0700

    fixed excercise 8

--- excercise-8.js
+++ excercise-8.js
@@ -1,10 +1,25 @@
 function tukarBesarKecil(kalimat) {
-    // you can only write your code here!
-  }
-  
-  // TEST CASES
-  console.log(tukarBesarKecil('Hello World')); // "hELLO wORLD"
-  console.log(tukarBesarKecil('I aM aLAY')); // "i Am Alay"
-  console.log(tukarBesarKecil('My Name is Bond!!')); // "mY nAME IS bOND!!"
-  console.log(tukarBesarKecil('IT sHOULD bE me')); // "it Should Be ME"
-  console.log(tukarBesarKecil('001-A-3-5TrdYW')); // "001-a-3-5tRDyw"
+    var kalimatUppercase = kalimat.toUpperCase();
+    var kalimatLowercase = kalimat.toLowerCase();
+    var kalimatArr = kalimat.split('');
+    for (var i = 0; i < kalimat.length; i++) {
+        if (kalimat[i] != kalimatUppercase[i]) {
+            kalimatArr.splice(i,1,kalimatUppercase[i]);
+        } else if (kalimat[i] != kalimatLowercase) {
+            kalimatArr.splice(i,1,kalimatLowercase[i]);
+        }
+    }
+    return kalimatArr.join('');
+}
+
+// TEST CASES
+
+console.log(tukarBesarKecil('Hello World')); // "hELLO wORLD"
+
+console.log(tukarBesarKecil('I aM aLAY')); // "i Am Alay"
+
+console.log(tukarBesarKecil('My Name is Bond!!')); // "mY nAME IS bOND!!"
+
+console.log(tukarBesarKecil('IT sHOULD bE me')); // "it Should Be ME"
+
+console.log(tukarBesarKecil('001-A-3-5TrdYW')); // "001-a-3-5tRDyw"
