07d2cb99003a6afccaa037fd9fe0d67e08f8658f
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-12 17:41:39 +0700

    fixed excercise 6

--- excercise-6.js
+++ excercise-6.js
@@ -1,5 +1,27 @@
 function digitPerkalianMinimum(angka) {
-    // you can only write your code here!
+    var pembagi = [];
+    var pengali = [];
+    var gabungan = [];
+    
+    // var lengthAngka = String(angka).length;
+    for (var i = 1; i <= angka; i++) {
+        if (angka % i === 0) {
+            pembagi.push(String(i));
+        }
+    }
+    for (var j = 0; j < pembagi.length; j++) {
+        pengali.push(String(angka/pembagi[j]));
+    }
+    for (var k = 0; k < pembagi.length; k++) {
+        gabungan.push([pembagi[k]+pengali[k]]);
+    }
+    var lengthArr = gabungan[0][0].length;
+    for (var l = 1; l < gabungan.length; l++) {
+        if (gabungan[l][0].length < lengthArr) {
+            lengthArr = gabungan[l][0].length;
+        }
+    }
+    return lengthArr;
 }
 // TEST CASES
 console.log(digitPerkalianMinimum(24)); // 2
